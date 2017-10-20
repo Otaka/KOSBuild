7161880343344e64459267d86ee9004a3e462db1
Dmitry <dsavchenko@inovvo.com>
2017-10-20 13:11:58 +0300

    PropertiesManager with other configuration management implementation PlaceholderResolver implementation sms action test

---
+++ VisualEagle/src/main/java/org/visualeagle/project/ProjectLoader.java
@@ -0,0 +1,8 @@
+package org.visualeagle.project;
+
+/**
+ * @author Dmitry
+ */
+public abstract class ProjectLoader {
+
+}
---
+++ VisualEagle/src/main/java/org/visualeagle/project/vnodes/VirtualFileVNode.java
@@ -0,0 +1,13 @@
+package org.visualeagle.project.vnodes;
+
+/**
+ * @author Dmitry
+ */
+public class VirtualFileVNode extends AbstractVNode {
+
+    @Override
+    public boolean isLeaf() {
+        return true;
+    }
+
+}
