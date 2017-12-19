1a6b95723d0211d8693d3d54051f9da574bb9130
Dmitry <dsavchenko@inovvo.com>
2017-12-19 17:53:33 +0200

    work on remoteCommander

---
+++ VisualEagle/src/test/java/org/visualeagle/gui/remotewindow/fileprovider/RFileTest.java
@@ -0,0 +1,15 @@
+package org.visualeagle.gui.remotewindow.fileprovider;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author sad
+ */
+public class RFileTest {
+
+    public RFileTest() {
+    }
+
+    
+}
