96d37e83dcb2a8bb1606ba9a7aa3df448d4db6f0
Dmitry <dsavchenko@inovvo.com>
2018-02-09 10:30:59 +0200

    remote emulator fix

--- VisualEagle/src/main/java/org/visualeagle/gui/remotewindow/fileprovider/RemoteFileSystemFileProvider.java
+++ VisualEagle/src/main/java/org/visualeagle/gui/remotewindow/fileprovider/RemoteFileSystemFileProvider.java
@@ -2,16 +2,25 @@ package org.visualeagle.gui.remotewindow.fileprovider;
 
 import com.asyncsockets.ListenableFutureTask;
 import com.asyncsockets.ListenableFutureTaskWithData;
-import java.nio.charset.StandardCharsets;
+import com.asyncsockets.Request;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.SwingUtilities;
 import org.visualeagle.gui.remoteconnection.RemoteConnectionManager;
+import org.visualeagle.utils.ByteArrayFormatter;
+import org.visualeagle.utils.ByteArrayParserFormatter;
 import org.visualeagle.utils.Lookup;
+import org.visualeagle.utils.Utils;
 
 /**
  * @author sad
  */
 public class RemoteFileSystemFileProvider extends AbstractFileProvider {
+
+    private int defaultTimeout = 100_000;
+    private String ERROR = "5";
+    private String OK = "1";
 
     @Override
     public String separator() {
@@ -28,22 +37,97 @@ public class RemoteFileSystemFileProvider extends AbstractFileProvider {
 
     @Override
     public ListenableFutureTask<List<RFile>> listFiles(RFile folder) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            ListenableFutureTaskWithData<List<RFile>> future = new ListenableFutureTaskWithData<>();
+            RemoteConnectionManager connectionManager = getConnectionManager(future);
+            ByteArrayFormatter byteFormatter = new ByteArrayFormatter();
+            byteFormatter.sendString("LIST_FLDER");
+            connectionManager.getCurrentConnection().writeWithExpectingResult(0, byteFormatter.getBytes(), defaultTimeout, (requestObject) -> {
+                try {
+                    ByteArrayParserFormatter parser = new ByteArrayParserFormatter(((Request) requestObject).getBytes());
+                    String ok = parser.receiveString();
+                    if (ok.equals(ERROR)) {
+                        String errorMessage = parser.receiveString();
+                        future.finishFutureAndReturnException(new IllegalArgumentException("Client send a message [" + errorMessage + "]"));
+                        System.out.println("Client send a message [" + errorMessage + "]");
+                        return;
+                    } else if (!ok.equals(OK)) {
+                        future.finishFutureAndReturnException(new IllegalArgumentException("Client has responded not with [OK], but with [" + ok + "]"));
+                        return;
+                    }
+
+                    List<RFile> children = new ArrayList<>();
+                    int count = parser.receiveInt();
+                    for (int i = 0; i < count; i++) {
+                        String name = parser.receiveString();
+                        boolean isDirectory=parser.receiveBoolean();
+                        long size=parser.receiveLong();
+                        long lastModified=parser.receiveLong();
+                        RFile childFile = new RFile(folder.getFullPath(), name,size, isDirectory, lastModified, this);
+                        children.add(childFile);
+                    }
+
+                    future.finishFutureAndReturnData(children);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    future.finishFutureAndReturnException(ex);
+                }
+            }, (a) -> {
+                SwingUtilities.invokeLater(() -> {
+                    Utils.showErrorMessage("Error while try to get list of root folders in remote file system [" + a + "]");
+                });
+            });
+            return future;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
     }
 
     @Override
     public ListenableFutureTask<List<RFile>> listRoots() {
-        ListenableFutureTaskWithData<List<RFile>> future = new ListenableFutureTaskWithData<>();
-        List<RFile> roots = new ArrayList<>();
+        try {
+            ListenableFutureTaskWithData<List<RFile>> future = new ListenableFutureTaskWithData<>();
+            RemoteConnectionManager connectionManager = getConnectionManager(future);
+            ByteArrayFormatter byteFormatter = new ByteArrayFormatter();
+            byteFormatter.sendString("LIST_ROOTS");
+            connectionManager.getCurrentConnection().writeWithExpectingResult(0, byteFormatter.getBytes(), defaultTimeout, (requestObject) -> {
+                try {
+                    ByteArrayParserFormatter parser = new ByteArrayParserFormatter(((Request) requestObject).getBytes());
+                    String ok = parser.receiveString();
+                    if (ok.equals(ERROR)) {
+                        String errorMessage = parser.receiveString();
+                        future.finishFutureAndReturnException(new IllegalArgumentException("Client send a message [" + errorMessage + "]"));
+                        System.out.println("Client send a message [" + errorMessage + "]");
+                        return;
+                    } else if (!ok.equals(OK)) {
+                        future.finishFutureAndReturnException(new IllegalArgumentException("Client has responded not with [OK], but with [" + ok + "]"));
+                        return;
+                    }
 
-        RemoteConnectionManager connectionManager = getConnectionManager(future);
-        connectionManager.getCurrentConnection().writeWithExpectingResult(0, "LIST_ROOTS".getBytes(StandardCharsets.UTF_8), 10_000, (a) -> {
-            System.out.println("A="+a);
-        }, (a) -> {
-            System.out.println("Error A="+a);
-        });
-        future.finishFutureAndReturnData(roots);
-        return future;
+                    List<RFile> roots = new ArrayList<>();
+                    int count = parser.receiveInt();
+                    for (int i = 0; i < count; i++) {
+                        String path = parser.receiveString();
+                        RFile rootFile = new RFile(null, path, 0, true, 0, this);
+                        roots.add(rootFile);
+                    }
+
+                    future.finishFutureAndReturnData(roots);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    future.finishFutureAndReturnException(ex);
+                }
+            }, (a) -> {
+                SwingUtilities.invokeLater(() -> {
+                    Utils.showErrorMessage("Error while try to get list of root folders in remote file system [" + a + "]");
+                });
+            });
+            return future;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
     }
 
     @Override
